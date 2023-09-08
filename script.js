b86b60a80f6cc8dc0bf90e0df7d1e4d0cfe5d76d
Simon Prophet <simon.s.prophet@outlook.com>
2023-09-08 07:27:37 +0100

    add JS code to update big cup

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 'use strict';
 
 const smallCups = document.querySelectorAll('.cup-small');
-const liters = document.querySelector('liters');
+const liters = document.querySelector('#liters');
 const percentage = document.querySelector('#percentage');
 const remained = document.querySelector('#remained');
+
+updateBigCup();
 
 smallCups.forEach((cup, idx) => {
   cup.addEventListener('click', () => highlightCups(idx));
@@ -24,4 +26,31 @@ function highlightCups(idx) {
       cup.classList.remove('full');
     }
   });
+
+  updateBigCup();
+}
+
+function updateBigCup() {
+  const fullCups = document.querySelectorAll('.cup-small.full').length;
+  const totalCups = smallCups.length;
+
+  if (fullCups === 0) {
+    percentage.style.visibility = 'hidden';
+    percentage.style.height = 0;
+  } else {
+    percentage.style.visibility = 'visible';
+    percentage.style.height = `${(fullCups / totalCups) * 330}px`;
+    console.log(
+      (percentage.style.height = `${(fullCups / totalCups) * 330}px`)
+    );
+    percentage.innerText = `${(fullCups / totalCups) * 100}%`;
+  }
+
+  if (fullCups === totalCups) {
+    remained.style.visibility = 'hidden';
+    remained.style.height = 0;
+  } else {
+    remained.style.visibility = 'visible';
+    liters.innerText = `${2 - (250 * fullCups) / 1000}L`;
+  }
 }
